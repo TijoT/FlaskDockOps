691c0a141c538510e00c4f79aca95ba77fda48b9
Tijo <tijo.thomas@gmx.de>
2024-10-14 00:10:35 +0200

    Expose port 3000 for flask app

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
 from server.webapp import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=3000)
--- src/server/webapp.py
+++ src/server/webapp.py
@@ -4,7 +4,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'Congrats !!'
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
